9fd2d35387e373b5610d034e4b681212ecec6c5a
Denis Shirokov <de.sh.vit@gmail.com>
2022-02-12 18:00:46 +0300

    add scripts and debot init template

---
+++ src/main/solidity/App.sol
@@ -0,0 +1,36 @@
+pragma ton -solidity >=0.35.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+
+import "./interfaces/Debot.sol";
+import "./interfaces/Terminal.sol";
+
+contract TezosWallet is Debot {
+
+    /// @notice Entry point function for DeBot.
+    function start() public override {
+        Terminal.print(0, "Hello!");
+    }
+
+    /// @notice Returns Metadata about DeBot.
+    function getDebotInfo() public functionID(0xDEB) override view returns (
+        string name, string version, string publisher, string caption, string author,
+        address support, string hello, string language, string dabi, bytes icon
+    ) {
+        name = "Tezos Wallet Bot";
+        version = "0.0.1";
+        publisher = "ShiroKovka";
+        caption = "";
+        author = "ShiroKovka";
+        support = address.makeAddrStd(0, 0xfe9a76f1a8584fbd8f092b20e917918969fc8a7b1759e9a8c15a7f907e4d72a5);
+        hello = "Hello, I am a DeBot. ShiroKovka(Oba!=)";
+        language = "en";
+        dabi = m_debotAbi.get();
+        icon = "";
+    }
+
+    function getRequiredInterfaces() public view override returns (uint256[] interfaces) {
+        return [Terminal.ID];
+    }
+}
